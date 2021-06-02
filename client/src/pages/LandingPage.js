40691dff2a9d23709f8c94060684a303ee283bcd
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-02 15:01:07 -0500

    Updated folder structure.

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CheckoutForm from '../components/CheckoutForm';
+import Three from  '../components/Three';
+import ThreeModel from  '../components/ThreeModel';
+import ConfettiGenerator from "confetti-js";
+
+
+const LandingPage = () => {
+  const [confettiSwitch, toggleConfetti] = React.useState(false);
+
+  // const canvas = document.createElement('canvas');
+  // const div = document.querySelector("div");
+  // div.appendChild(canvas);
+  // canvas.setAttribute({ id: "canvas" });
+  // const confetti = new ConfettiGenerator(canvas);
+  // if (confettiSwitch) {
+  //   confetti.render();
+  // } else if (!confettiSwitch) {
+  //   // confetti.clear();
+  // }
+
+  return (
+    <div>
+      {/* <canvas id="canvas" ></canvas> */}
+      <h1 style={{fontSize:"160px"}}>Landing</h1>
+      <CheckoutForm toggleConfetti={toggleConfetti} />
+      <Three />
+    </div>
+  )
+}
+
+export default LandingPage
