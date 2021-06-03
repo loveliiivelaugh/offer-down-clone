5416239fb12780095e01aadb5715066aa4429204
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-03 07:34:42 -0500

    Removed accidentally left boilerplate express route in server.

--- server.js
+++ server.js
@@ -7,23 +7,17 @@ const bodyParser = require("body-parser");
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json())
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-
-
 //Stripe route to accept payments
 app.post('/pay', async (req, res) => {
   // Set your secret key. Remember to switch to your live secret key in production.
   // See your keys here: https://dashboard.stripe.com/apikeys
-  console.log(req.body)
+  console.log(req.body);
   const { email } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 1099,//todo --> Need to update this to grab the price from the db within the server to maintain security.
@@ -37,9 +31,6 @@ app.post('/pay', async (req, res) => {
   res.json({ client_secret: paymentIntent.client_secret });
 });
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
