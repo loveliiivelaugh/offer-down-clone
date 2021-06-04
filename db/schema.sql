a0718f94ea300844a221b056fb31206f56ce3257
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-04 12:22:34 -0500

    editing

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,3 @@ USE offer_downDB;
 
 
 
-product has to get tied to user so only a user can out product up 
-product should have list of offers from another user.
-user = sold products, and posted products and email, password, and login
-name address city state zip
-data validation
-product details(go over)
-
-keep track of pending offers and each product has form other user
-
