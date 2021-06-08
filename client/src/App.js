89f926f8bbad2f7ac35c681cbe78792a0c21dcac
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-08 11:02:10 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,22 @@ import { ProvideAuth } from "./hooks/useAuth.js";
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
+const useStyles = makeStyles((theme) => ({}));
+export default function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>OfferDown</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
