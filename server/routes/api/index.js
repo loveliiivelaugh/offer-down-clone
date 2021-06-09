51c5af5a24d12e40894eef7543c3410a47de6491
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-09 06:09:32 -0500

    Uncommented userRoutes to

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 // const stripeRoutes = require('./stripeRoutes.js');
 // const plaidRoutes = require('./plaidRoutes.js');
-// const userRoutes = require('./userRoutes.js');
+const userRoutes = require('./userRoutes.js');
 // const productRoutes = require('./productRoutes.js');
 
 // router.get('/stripe', stripeRoutes);
 // router.get('/plaid', plaidRoutes);
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 // router.use('/products', productRoutes);
 
 
