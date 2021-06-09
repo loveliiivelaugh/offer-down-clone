dc07ebc1741e0297ccd8995460b7b8cf155eca68
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-08 20:28:33 -0500

    Uncommented userRoutes to see if working with Heroku.

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,124 +1,126 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
-const Product = require('../../models/Product');
 
-
-// @method: GET /api/users/:id
-// @descr: Return select user by id
-// @API getUser()
-router.get('/:id', async (req, res) => {
-  try {
-    //get your model
-
-    const userData = await User.find({})
-    console.log(userData)
-
-    //return a rersponse code and json object.
-    res.status(200).json(userData[0]);
-  } catch (error) {
-    res.status(500).json({ error: error });
-  }
-
-});
-
-// @method: GET /api/users/:id
-// @descr: Return all the users in database
-// @API: getUsers()
-router.get('/', (req, res) => {});
-// Get one user
-router.get('/:id', async (req, res) => {
-  try {
-    const userData = await User.findOne({ id: req.params.id });
-
-    res.status(200).json(userData);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
-
-});
-
-// get all users
+// const router = express.Router();
 router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll({});
+  res.json({ message: "Success" });
+})
 
-    res.status(200).json(userData);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
-});
+// // @method: GET /api/users/:id
+// // @descr: Return select user by id
+// // @API getUser()
+// router.get('/:id', async (req, res) => {
+//   try {
+//     //get your model
 
-//createUser()
-router.post('/', async ({ body }, res) => {
-  try {
-    const newUser = await User.create(body);
+//     const userData = await User.find({})
+//     console.log(userData)
 
-    console.log(body);
+//     //return a rersponse code and json object.
+//     res.status(200).json(userData[0]);
+//   } catch (error) {
+//     res.status(500).json({ error: error });
+//   }
 
-    newUser 
-      ? res.status(200).json(newUser) 
-      : res.status(500).json({ error: "Somethings wrong?!" });
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
+// });
 
-});
+// // @method: GET /api/users/:id
+// // @descr: Return all the users in database
+// // @API: getUsers()
+// router.get('/', (req, res) => {});
+// // Get one user
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ id: req.params.id });
+
+//     res.status(200).json(userData);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
+
+// });
+
+// // get all users
+// router.get('/', async (req, res) => {
+//   try {
+//     const userData = await User.findAll({});
+
+//     res.status(200).json(userData);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
+// });
+
+// //createUser()
+// router.post('/', async ({ body }, res) => {
+//   try {
+//     const newUser = await User.create(body);
+
+//     console.log(body);
+
+//     newUser 
+//       ? res.status(200).json(newUser) 
+//       : res.status(500).json({ error: "Somethings wrong?!" });
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
+
+// });
 
 
-// @method: POST /api/likes/:id
-// @descr: Return the current logged in users saved items
-// @API addLikedItem()
-router.post('/likes/:id', async ({ body }, res) => {
-  try {
-    const user = await User.find({});
+// // @method: POST /api/likes/:id
+// // @descr: Return the current logged in users saved items
+// // @API addLikedItem()
+// router.post('/likes/:id', async ({ body }, res) => {
+//   try {
+//     const user = await User.find({});
 
-    console.log(body);
+//     console.log(body);
 
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
-});
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
+// });
 
-//addLikedItem()
-//On the front end, we also send the id of the current user along with this post request
-router.post('/likes/:id', async (req, res) => {
-  try {
-    const product = await Product.find({ _id: req.params.id });
-    const user = await User.findOne({ id: req.body.user });
+// //addLikedItem()
+// //On the front end, we also send the id of the current user along with this post request
+// router.post('/likes/:id', async (req, res) => {
+//   try {
+//     const product = await Product.find({ _id: req.params.id });
+//     const user = await User.findOne({ id: req.body.user });
 
-    user.saved_items.push(product);
+//     user.saved_items.push(product);
 
-    const updatedUser = await User.updateOne({ id: req.body.user }, { user });
+//     const updatedUser = await User.updateOne({ id: req.body.user }, { user });
 
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
+//     res.status(200).json(updatedUser);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
 
-});
+// });
 
-router.put('/:id', async (req, res) => {
-  console.log(req.body);
-  try {
-    const userData = await User.updateOne({ id: req.params.id }, req.body); // talk with team
+// router.put('/:id', async (req, res) => {
+//   console.log(req.body);
+//   try {
+//     const userData = await User.updateOne({ id: req.params.id }, req.body); // talk with team
 
-    res.status(200).json(userData);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  };
+//     res.status(200).json(userData);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   };
 
-});
+// });
 
-router.delete('/:id', async (req, res) => { 
-  try {
-    const userData = await User.remove({ id: req.params.id });
+// router.delete('/:id', async (req, res) => { 
+//   try {
+//     const userData = await User.remove({ id: req.params.id });
 
-    res.status(200).json(userData);
-  } catch (error) {
-    res.status(500).json({ errorMessage: error });
-  }
-});
+//     res.status(200).json(userData);
+//   } catch (error) {
+//     res.status(500).json({ errorMessage: error });
+//   }
+// });
 
 module.exports = router;
