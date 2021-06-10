0bf17eeed6c5659e2a968742651ba50c1816b247
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-10 13:33:43 -0500

    changed file name

--- client/src/components/BuyandSell.js
+++ client/src/components/BuyandSell.js
@@ -18,7 +18,7 @@ export default function () {
   const classes = useStyles();
   const checked = useWindowPosition('header');
   return (
-    <div className={classes.root} id="buy-sell-offer">
+    <div className={classes.root} id="buy-sell">
       <ImageCard place={places[1]} checked={checked} />
       <ImageCard place={places[0]} checked={checked} />
     </div>
