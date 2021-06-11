7d52f08fad6bfda7534ed312d1b45e182eb442b2
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-11 11:36:54 -0500

    index.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import { ProvideAuth } from "./hooks/useAuth.js";
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import BuyandSell from './components/BuyandSell';
+
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -17,6 +22,24 @@ function App() {
         <Pages />
       </Elements>
     </ProvideAuth>
+  );
+}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/offerdownbackground.jpeg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+export default function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <BuyandSell />
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.unregister();
